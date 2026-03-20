889afe6341efc98fa06f2c1f0a9a2d818f9c1745
Pablo Nicolas  Segato <pablo.segato47@gmail.com>
2026-03-20 20:55:26 +0100

    Continuo con el estilizado de la APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 import "./app.css";
 import { Cards } from "./pages/Cards";
 import { Pagination } from "./pages/Pagination";
-
 export const App = () => {
   return (
     <div className="container">
-      <h1>LOS SIMPSON</h1>
+      <h1>LOS SIMPSONS</h1>
       <h2>APP - SPRINGFIELD DATABASE</h2>
       <ul>
-        <li>Personajes </li>
-        <li>Páginas </li>
+        {/* pasar informacion del fetch con un contexto  de total personajes y paginas */}
+
+        <li>
+          <span>1182</span>Personajes{" "}
+        </li>
+        <li>
+          <span>68</span>Páginas{" "}
+        </li>
         <li>Springfield, USA</li>
       </ul>
 
-      <div className="search-seccion">
-        {" "}
-        <form action="">
-          <input type="text" />
-          <button>TODOS </button>
-          <button>MALE</button>
-          <button>FEMALE </button>
-          <button>ALIVE</button>
-        </form>
-      </div>
-      <span>
-        <h3>PERSONAJES - PÁGINA 1</h3>
-      </span>
+      <form className="search-seccion">
+        <input type="text" placeholder="Buscar personaje ..." />
+        <button>TODOS </button>
+        <button>MALE</button>
+        <button>FEMALE </button>
+        <button>ALIVE</button>
+      </form>
+      <h3 id="title-pagination">PERSONAJES - PÁGINA 1</h3>
       <Cards />
       <Pagination />
 
