333b31c211a9db659e68308c287a3e04d54c9d22
Pablo Nicolas  Segato <pablo.segato47@gmail.com>
2026-03-21 15:31:21 +0100

    funcionalidades del boton de Ver frase y agregado de cerrar

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,30 +1,24 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./cards.css";
+import { useFetch } from "../hooks/useFetch.jsx";
+import { url } from "../constants.js";
 
 export const Cards = () => {
   const [pages, setPages] = useState(1);
-  const url = `https://thesimpsonsapi.com/api`;
   const API_URL = `${url}/characters?page=${pages}`;
-  const [data, setData] = useState([]);
+  const [activeId, setActiveId] = useState(null);
+
+  const handlePhrase = (id) => {
+    setActiveId(activeId === id ? null : id);
+  };
+  const handleClose = () => {
+    setActiveId(null);
+  };
 
   // Colocar todas las variables posibles en un archivo de constantes.js para poder hacer mas optimo el codigo
 
-  // Definimos la función asíncrona
-  useEffect(() => {
-    const obtenerDatos = async () => {
-      try {
-        const respuesta = await fetch(API_URL);
-        const res = await respuesta.json();
-        setData(res); // Guardamos los datos en el estado
-      } catch (error) {
-        console.error("Hubo un error al pedir los datos:", error);
-      }
-    };
-    // pasar esto un hook.js para optimizar codigo
-    obtenerDatos();
-  }, [pages]);
+  const { data } = useFetch(API_URL);
 
-  console.log(data);
   return (
     <div className="container-cards">
       {data.results?.map((personajes) => (
@@ -42,20 +36,35 @@ export const Cards = () => {
           <div className="card-body">
             <h3>{personajes.name}</h3>
             <span>
-              {personajes.gender} AGE {personajes.age}
+              {personajes.gender} ~ AGE {personajes.age}
             </span>
             <div className="ocupation-text">{personajes.occupation}</div>
-            <button className="phrases-toggle">VER FRASES </button>
 
-            <div className="phrases">
-              {/* Aca lo que tengo que hacer es que el botton me cambie el display para ver o no las frases */}
-              {personajes.phrases.map((phrases, index) => (
-                <div className="container-phrases">
-                  {" "}
-                  <p key={index}> {phrases}</p>{" "}
-                </div>
-              ))}
-            </div>
+            <button
+              className="phrases-toggle"
+              onClick={() => {
+                handlePhrase(personajes.id);
+              }}
+            >
+              VER FRASES{" "}
+            </button>
+            {activeId == personajes.id && (
+              <div className="phrases">
+                {personajes.phrases.map((phrases, index) => (
+                  <div className="container-phrases" key={index}>
+                    {" "}
+                    <p key={index}> {phrases}</p>
+                  </div>
+                ))}
+                <button
+                  className="phrases-toggle"
+                  style={{ marginTop: "5px" }}
+                  onClick={handleClose}
+                >
+                  close
+                </button>
+              </div>
+            )}
           </div>
         </div>
       ))}
---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (API_URL) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const obtenerDatos = async () => {
+      try {
+        const respuesta = await fetch(API_URL);
+        const res = await respuesta.json();
+        setData(res);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    obtenerDatos();
+  }, [API_URL]);
+
+  return { data };
+};
